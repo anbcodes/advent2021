0441e47fff3e7246ca9f4a5bf52e84b26aacc250
Anbcodes <anbcodes@protonmail.com>
2021-12-08 09:06:26 -0500

    more days

--- day1/day1.go
+++ day1/day1.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Go: Not implemented")
+	fmt.Println("Go:")
+	content, err := ioutil.ReadFile("text.txt") // the file is inside the local directory
+	if err != nil {
+		fmt.Println("Error reading file")
+	}
+
+	str := string(content)
+
+	input := strings.Split(str, "\n")
+
+	total = 0
+	for i, v := range input {
+		value_number, err = strconv.Atoi(v);
+		if ()
+		if i > 0 &&  > input[i-1] {
+			total += 1
+		}
+	}
 }
